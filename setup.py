11c472b4a6072c037594fb7ba05e174d584d7bcc
Victor Liu <victor.liu@gmail.com>
2018-08-31 11:43:41 -0700

    More intelligent detection of glfw

--- setup.py
+++ setup.py
@@ -2,12 +2,15 @@ from distutils.core import setup, Extension
 from sys import platform
 import os
 
-libraries = ['glfw3'];
+libraries = [];
 if platform == 'darwin':
+	libraries.append('glfw');
 	os.environ['LDFLAGS'] = '-framework Cocoa -framework OpenGL -framework IOKit -framework CoreFoundation -framework CoreVideo';
 elif platform == 'win32':
+	libraries.append('glfw3');
 	libraries.append('opengl32');
 elif platform == 'linux':
+	libraries.append('glfw3');
 	libraries.append('GL');
 
 PyGeom2 = Extension('PyGeom2',
